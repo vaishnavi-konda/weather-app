388894f3ffef96ea3b0c6b992ff0e024776c9341
Vaishnavi Konda <kvaishnavi026@gmail.com>
2021-12-25 19:19:21 +0530

    Remove console.logs

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.post('/', function (req, res) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
   https.get(url, function (response) {
-    console.log(response.statusCode, response.statusMessage);
+    // console.log(response.statusCode, response.statusMessage);
 
     response.on('data', function (data) {
       // we get data from weather api in the form of string(hexadecimal).
@@ -26,9 +26,7 @@ app.post('/', function (req, res) {
       const temp = weatherData.main.temp;
       const weatherDesc = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
-      console.log(icon);
       const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-      console.log(temp, weatherDesc);
 
       res.write(
         `<h1>The temperature in ${city} is ${temp} degree celsius.</h1>`
@@ -38,8 +36,6 @@ app.post('/', function (req, res) {
       res.send();
     });
   });
-
-  // res.send('Server is running');
 });
 
 app.listen(3000, function () {
