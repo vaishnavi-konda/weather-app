aff6d8632202a9295c4abe018b943d8b035ebc37
Vaishnavi Konda <kvaishnavi026@gmail.com>
2022-04-20 20:44:21 +0530

    Update response pages

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
 app.post('/', function (req, res) {
   const city = req.body.cityName;
   const apiKey = '9c49f3bddc02a39b727bf68e5b35d12f';
@@ -17,10 +13,8 @@ app.post('/', function (req, res) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
   https.get(url, function (response) {
-    // console.log(response.statusCode, response.statusMessage);
-
     if (response.statusCode !== 200) {
-      res.sendFile(__dirname + '/failure.html');
+      res.redirect('/failure');
     } else {
       response.on('data', function (data) {
         // we get data from weather api in the form of string(hexadecimal).
@@ -32,22 +26,31 @@ app.post('/', function (req, res) {
         const icon = weatherData.weather[0].icon;
         const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
-        res.write(
-          `<h1>The temperature in ${city} is ${temp} degree celsius.</h1>`
-        );
-        res.write(`<h2>The weather is ${weatherDesc}</h2>`);
-        res.write(`<img src=${iconUrl}>`);
-        res.send();
+        res.send(`<form action="/" method="get">
+        <h1>The temperature in ${city} is ${temp} degree celsius.</h1>
+        <h2>The weather is ${weatherDesc}</h2>
+        <img src=${iconUrl}>
+        <button>Try Another city</button>
+        </form>`);
       });
     }
   });
 });
 
-app.post('/failure', function (req, res) {
-  var a = 10;
-  res.redirect('/');
+app.get('/failure', function (req, res) {
+  // res.redirect('/');
+  res.send(`<div class="mt-4 p-5 bg-primary text-white rounded">
+  Sorry, the weather details of this city are not available. Please try
+  another city.
+  <form action="/" method="get">
+  <button class="btn btn-light" type="submit">Try Again</button>
+  </form>
+  </div>`);
 });
 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 app.listen(3000, function () {
   console.log('listening on port 3000...');
 });
